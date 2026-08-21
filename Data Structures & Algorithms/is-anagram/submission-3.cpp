dd4276d9202669a25274b26be776ccf958aa0de4
SiddanthN <91083861+SiddanthN@users.noreply.github.com>
2026-08-21 20:19:04 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,64 @@
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    
+    unordered_map<char, int> seen_chars_list;
+
+    bool isAnagram(string s, string t) {
+
+        if (s.length() != t.length()) {
+
+            seen_chars_list.clear();
+            return false;
+
+        };
+        
+        getCharCount(s);
+        getCharCount(t, true);
+        return checkDiffCount();
+
+    }
+
+    void getCharCount(string s, bool start_diff=false) {
+
+        for(char c: s) {
+
+            if(start_diff) {
+
+                seen_chars_list[c] = seen_chars_list[c] - 1;
+
+            } else {
+
+                if(!seen_chars_list.contains(c)) {
+
+                    seen_chars_list[c] = 0;
+
+                }
+
+                seen_chars_list[c] = seen_chars_list[c] + 1;
+
+            }
+
+        }
+
+    }
+
+    bool checkDiffCount() {
+
+        for(const auto& pair: seen_chars_list) {
+
+            if(pair.second != 0) {
+
+                return false;
+
+            };
+
+        }
+
+        return true;
+
+    }
+
+};
